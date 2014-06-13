cd3ecf6f7a98e3666a35a00de78917a374a96c77
shishu <smomal@qq.com>
2014-06-13 15:30:40 +0800

    update Makefile for sgd

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,9 @@
 #include "constant.h"
+#include "stdio.h"
 
 int main()
 {
-    printf("start sgd for lr");
+    printf("start sgd for lr[%u]\n", shishu::sgd::MAX_INPUT_LENGTH);
+    long start = shishu::sgd::MAX_INPUT_LENGTH;
     return 0;
 }
